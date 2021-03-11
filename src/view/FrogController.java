1e51822799e90f18e67ffacf4a81e11e376e8799
Marcio Chaves de Aguiar <jacksonmarcious@hotmail.com>
2021-03-11 00:20:25 -0300

    AulaThreads2 Commit

---
+++ src/view/FrogController.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.util.concurrent.Semaphore;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+public class FrogController {
+
+	public FrogController(Semaphore farol, JLabel lbFrog1, JLabel lbFrog2, JLabel lbFrog3, JLabel lbFrog4,
+			JLabel lbFrog5, JButton btnIniciar, JTextArea textArea, JLabel lbFinish, JTextArea textAreaFrog1,
+			JTextArea textAreaFrog2, JTextArea textAreaFrog3, JTextArea textAreaFrog4, JTextArea textAreaFrog5) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
